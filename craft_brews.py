c20820d5dd398d8df7b04e7da7f9ba6c5fdf781f
malinquentFlyyyer <69125322+malinquentFlyyyer@users.noreply.github.com>
2020-11-16 15:48:22 -0500

    Add files via upload

---
+++ craft_brews.py
@@ -0,0 +1,40 @@
+# allow a customer to issue a bar request
+
+bar_order = [
+                {
+                    'id': 1,
+                    'craft': 'Russian Imperial',
+                    'brand': 'Dosvidanya',
+                    'location': "Milwaukee Brewery"
+                },
+                {
+                    'id': 2,
+                    'craft': 'American Stout',
+                    'brand': 'Shakespeare Stout',
+                    'location': "Oregon Craft Beers"
+                    
+                },
+                
+                {
+                    'id' : 3,
+                    'craft' : 'German-Style Schwarzbier',
+                    'brand': 'Black Bavarian',
+                    'location': "Sprecher Brewing Co."
+
+                },
+
+                {
+                    'id': 4,
+                    'craft': 'Belgian-style Dubbel',
+                    'brand': 'Abbey Dubbel',
+                    'location': "Elm City Brewing Co."
+                },
+
+                {
+                    'id': 5,
+                    'craft': 'Irish-style Dry Stout',
+                    'brand': 'O.V.L. Stout',
+                    'location': "Russian River Brewing Co."
+                }
+
+]
